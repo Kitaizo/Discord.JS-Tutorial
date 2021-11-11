37634a873eed9e75785b4aab953edf2d20e644d9
Nguyễn Đức Khải <33801881+DiozVN@users.noreply.github.com>
2021-11-11 18:47:16 +0700

    Create ping.js

---
+++ commands/infomation/ping.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'ping',
+    category: 'infomation',
+    description: 'Show ping bot!',
+
+    run: async(client, message, args) => {
+        const msg = await message.channel.send('🏓 Checking ping...');
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`${client.user.username} Ping !`)
+            .setDescription(`Ping is: ${client.ws.ping} ms!\nMessage Ping is: ${Math.floor(msg.createdAt - message.createdAt)} ms!`)
+            .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+            .setTimestamp();
+        
+        await message.channel.send(embed);
+        msg.delete();
+    }
+}
